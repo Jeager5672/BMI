957a1394a86d417b2aeda954f37c32e67054eb2d
Barış Tüzün <tuzun94@gmail.com>
2024-02-21 17:36:56 +0300

    picture added and fixed issues

--- BMI.py
+++ BMI.py
@@ -1,18 +1,28 @@
+import tkinter
 from tkinter import *
+from PIL import ImageTk, Image
 
+path = r'C:\Users\tuzun\Desktop\Remove-bg.ai_1708437756203.png'
 window = Tk()
 window.title("BMI Calculater")
 window.minsize(width=400, height=400)
 window.config(padx=20, pady=20)  # boşluk bırakır
 
-my_label = Label(text="Vücut Kitle Endeksi", font=("Arial", 20, "italic"))
-my_label.config(fg="black")
-my_label.config(padx=10, pady=10)
+#resmin orjinalliğini koruyarak boyutlandırma
+original_image = Image.open(path)
+resized_image = original_image.resize((130, 130))
+
+img = ImageTk.PhotoImage(resized_image)
+
+image_label = Label(window, image=img) #picture added
+image_label.pack()
+
+my_label = tkinter.Label(text="BMI Calculator",font=("Arial",18),fg='saddle brown')
+my_label.config(pady=13)
 my_label.pack()
 
 my_label1 = Label(text="Kilonuzu Girin")
 my_label1.config(fg="black")
-my_label1.config(padx=10, pady=10)
 my_label1.pack()
 
 # kilo entry
@@ -57,11 +67,17 @@ def button():
             result_text = "Endeks: {:.1f} Normal Kilo".format(vki)
 
         result_label.config(text=result_text)
+        result_label.config(padx=10,pady=10)
     else:
         result_label.config(text="Geçerli bir sayı giriniz.")
 
 my_button = Button(text="Hesapla", command=button)
 my_button.config(padx=1, pady=1)
 my_button.pack(padx=(10, 0), pady=(10, 0))
+
+#koordinat
+image_label.update()
+
+print(image_label.winfo_x(),image_label.winfo_y())
 
 window.mainloop()
